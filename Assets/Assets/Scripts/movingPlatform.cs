89a0bfe53e0204f162e82053c209cc399eb6a9c0
Jessie Aniguid <jessieaniguid@gmail.com>
2018-05-26 22:16:23 -0700

    Added Moving Platforms. Lever Connection still not implemented

---
+++ Assets/Assets/Scripts/movingPlatform.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movingPlatform : MonoBehaviour {
+
+    public Transform targetLocation;
+    public Transform initialLocation;
+    public float platformSpeed;
+    public Transform temp;
+    public bool isHorizontal;
+    public bool isActive;
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isActive) return;
+		if(transform.position != targetLocation.position)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetLocation.position, platformSpeed);
+            //Debug.Log("Moving towards " + targetLocation.position.x);
+        }else if(isHorizontal && transform.position.x >= targetLocation.position.x)
+        {
+            //make current location equal to target location
+            //set target location to initial location
+            Swap();
+        }else if(!isHorizontal && transform.position.y >= targetLocation.position.y)
+        {
+            Debug.Log("Here");
+            Swap();
+        }
+	}
+
+    void Swap()
+    {
+        temp = targetLocation;
+        targetLocation = initialLocation;
+        initialLocation = temp;
+
+    }
+}
