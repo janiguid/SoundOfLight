87dff2098f9baac8aa6a5fad239e38f2b6c8e713
Jessie Aniguid <jessieaniguid@gmail.com>
2018-05-31 18:29:19 -0700

    Added everthing again. This has an intro

--- Assets/Assets/Scripts/introScroll.cs
+++ Assets/Assets/Scripts/introScroll.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class introScroll : MonoBehaviour
 {
+    public Light lightSource;
 
     public float speed;
     public float timer;
     public bool canMove;
     public float startX;
+    public float difference;
+    public float pauseLength;
+    public float sceneCounter;
+    public float lightDecrementor;
+
     // Use this for initialization
     void Start()
     {
@@ -16,35 +22,79 @@ public class introScroll : MonoBehaviour
         timer = 0;
         gamePause();
         startX = transform.position.x;
+        difference = 8;
+        pauseLength = 10;
+        lightDecrementor = 0.01f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (sceneCounter > 6)
+        {
+            canMove = false;
+            lightSource.range -= 0.01f;
+            return;
+        }
+        //if (sceneCounter == 8) return;
+
+        if (sceneCounter == 0)
+        {
+            pauseLength = 10;
+        }
+        else
+        {
+            pauseLength = 13;
+        }
+
+
         if(canMove == false)
         {
             gamePause();
         }
-        if(startX - transform.position.x >= 8)
+        if(startX - transform.position.x >= difference)
         {
             canMove = false;
             gamePause();
         }
-        if (canMove)
+        if (canMove && (sceneCounter < 7))
         {
             transform.position = new Vector3(transform.position.x - speed, transform.position.y, transform.position.z);
+        }
+
+        if(sceneCounter == 3)
+        {
+            if(lightSource.range > 11)
+            {
+                lightSource.range -= lightDecrementor;
+            }
+        }else if(sceneCounter == 4)
+        {
+            lightDecrementor = 0.005f;
+            if(lightSource.range > 10)
+            {
+                lightSource.range -= lightDecrementor;
+            }
+        }else if(sceneCounter == 5)
+        {
+            if(lightSource.range > 9.5)
+            {
+                lightSource.range -= lightDecrementor;
+            }
         }
     }
 
     void gamePause()
     {
-        timer += Time.deltaTime;
+        
+        timer += Time.fixedDeltaTime;
 
-        if (timer > 5)
+        if (timer > pauseLength)
         {
             canMove = true;
             timer = 0;
             startX = transform.position.x;
+            sceneCounter++;
         }
     }
 }
