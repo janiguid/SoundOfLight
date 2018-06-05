9db1a3ea32e4cf84c5288c6f5c79b30c6216f266
Jessie Aniguid <jessieaniguid@gmail.com>
2018-06-05 10:40:39 -0700

    Added changes for scripts

---
+++ Assets/Assets/SkyScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyScript : MonoBehaviour {
+
+    public SpriteRenderer spriteRenderer;
+    public SpriteRenderer[] spriteRendererToFade;
+    Color spriteColor;
+
+    public float fadeOutSpeed;
+    public int currentIndex;
+
+	// Use this for initialization
+	void Start () {
+       spriteRenderer = GetComponent<SpriteRenderer>();
+       fadeOutSpeed = 0.01f;
+       currentIndex = 0;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        spriteColor = spriteRenderer.color;
+        
+        if(spriteColor.a > 0 && currentIndex != 7)
+        {
+            spriteColor.a -= fadeOutSpeed;
+            spriteRenderer.color = spriteColor;
+        }
+        else if(spriteColor.a < 0 && currentIndex != 6)
+        {
+            currentIndex++;
+            spriteRenderer.sprite = spriteRendererToFade[currentIndex].sprite;
+            spriteRendererToFade[currentIndex].enabled = false;
+            spriteColor.a = 1;
+            spriteRenderer.color = spriteColor;
+        }
+        //Debug.Log(spriteColor.a);
+        
+    }
+}
---
+++ Assets/Assets/endingLyteScript.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class endingLyteScript : MonoBehaviour {
+
+
+
+    public Transform destination;
+    public Animator anim;
+
+
+    //public Animator animator;
+    public bool reachedDestination;
+
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if(destination.position.x <= transform.position.x && GetComponent<playerMovement>().isGrounded)
+        {
+            reachedDestination = true;
+        }
+
+        if (reachedDestination && GetComponent<playerMovement>().isGrounded)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+            GetComponent<playerMovement>().enabled = false;
+            GetComponent<playerSing>().enabled = false;
+            GetComponent<playerShoot>().enabled = false;
+            //GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+            anim.SetBool("Ending", true);
+            anim.Play("LyteIdle");
+        }
+    }
+    //// Use this for initialization
+    //void Start () {
+    //       isWalking = true;		
+    //}
+
+    //// Update is called once per frame
+    //void Update () {
+    //       if(transform.position.x != destination.position.x)
+    //       {
+    //           transform.position = Vector3.MoveTowards(transform.position, destination.position, 0.1f);
+    //           if(isWalking == true)
+    //           {
+    //               animator.Play("LyteWalk");
+    //               animator.SetBool("Moving", true);
+    //               isWalking = false;
+    //           }
+    //       }
+    //       else 
+    //       {
+    //           Debug.Log("wahtt");
+    //           animator.Play("LyteIdle");
+    //           animator.SetBool("Moving", false);
+
+    //       }
+
+    //       Debug.Log(transform.position.x + " " + destination.position.x);
+    //}
+}
