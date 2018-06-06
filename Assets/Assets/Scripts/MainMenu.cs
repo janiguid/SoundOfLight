1347d8d47a29d29e280fb9012c69a33040eedebb
MSI\Nick <nsgleaso@ucsc.edu>
2018-06-06 16:37:14 -0700

    modified main menu script and deleted copies of script

--- Assets/Assets/Scripts/MainMenu.cs
+++ Assets/Assets/Scripts/MainMenu.cs
@@ -5,9 +5,49 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
-    public void PlayGame()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
+    int indicator = 1;
 
+    private void update()
+    {
+
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (indicator == 4)
+                indicator = 1;
+            else
+                indicator += 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (indicator == 1)
+                indicator = 4;
+            else
+                indicator -= 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return) && indicator == 1) //play
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return) && indicator == 2) //Help
+        {
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return) && indicator == 3) //Credits
+        {
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return) && indicator == 4) //Quit
+        {
+            
+        }
+
+
+
+
+    }
 }
