01ad3dcf7cefa6b5d6de38f5a1d3ed3cec62aaec
Jessie Aniguid <jessieaniguid@gmail.com>
2018-06-06 00:57:02 -0700

    Added prefab that will restart level once Lyte falls off stage

---
+++ Assets/Assets/Scripts/grimReaper.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class grimReaper : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(collision.gameObject);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
